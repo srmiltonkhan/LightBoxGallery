2ce15e9c7a02df137081d022308dd5e958acf54f
Milton Khan <43887779+srmiltonkhan@users.noreply.github.com>
2021-08-08 21:05:17 +0600

    Add files via upload

---
+++ LightBoxGallery/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Light Box Image Gallery</title>
+	<link rel="stylesheet" type="text/css" href="css/lightbox.min.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+	<script type="text/javascript" src="js/lightbox-plus-jquery.min.js"></script>
+</head>
+<body>
+ <h1>Light Box Image Gallery</h1>
+ <div class="gallery">
+ 	<a href="images/1.jpg" data-lightbox="mygallery"><img src="images/1.jpg"></a>
+ 	<a href="images/2.jpg" data-lightbox="mygallery"><img src="images/2.jpg"></a>
+ 	<a href="images/2.jpg" data-lightbox="mygallery"><img src="images/2.jpg"></a>
+ 	<a href="images/2.jpg" data-lightbox="mygallery"><img src="images/2.jpg"></a>
+ 	<a href="images/2.jpg" data-lightbox="mygallery"><img src="images/2.jpg"></a>
+ 	<a href="images/2.jpg" data-lightbox="mygallery"><img src="images/2.jpg"></a>
+ 	<a href="images/2.jpg" data-lightbox="mygallery"><img src="images/2.jpg"></a>
+
+ </div>
+</body>
+</html>
